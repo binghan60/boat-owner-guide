48d9a144b19e1e00523f99cd0caa212c8a19cf8e
asher_wang <asher_wang@ekera.com.tw>
2025-05-09 11:28:33 +0800

    更新footer

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import HeaderComponent from '@/components/HeaderComponent.vue'
+import FooterComponent from '@/components/FooterComponent.vue'
 </script>
 
 <template>
   <HeaderComponent></HeaderComponent>
+  <footer-component></footer-component>
 </template>
